c4a2682bd9eb611d8870efd7851252548f00676d
Madone <ahamad.jedu@gmail.com>
2016-04-29 15:36:07 +0700

    Hello world

---
+++ newPHPClass.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace jehdu\mkeditor;
+
+/**
+ * Description of newPHPClass
+ *
+ * @author madone
+ */
+class newPHPClass {
+    //put your code here
+    public static function run(){
+        echo "test";
+    }
+}
